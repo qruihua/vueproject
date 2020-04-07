b1c3600b8719279be630496bda9468cea28defc2
qiruihua <qiruihua@itcast.cn>
2020-04-07 22:54:15 +0800

    elementui

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+// 导入ElementUI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 全局使用
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
