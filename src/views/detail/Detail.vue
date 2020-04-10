5a08e585466606b0bb881ff3e6d348d0dcddc346
qiruihua <qiruihua@itcast.cn>
2020-04-10 11:25:47 +0800

    shoptopservices

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,7 +2,12 @@
     <div>
       <!--第一行内容-->
       <el-row>
-        <el-col :span="8"><div class="grid-content bg-purple">1.1</div></el-col>
+        <!--门店服务总人数-->
+        <el-col :span="8">
+          <div class="grid-content bg-purple">
+            <car-pie :chartDesc="shopServicesDesc" :chartData="shopServicesData"></car-pie>
+          </div>
+        </el-col>
         <el-col :span="8"><div class="grid-content bg-purple-light">1.2</div></el-col>
         <el-col :span="8"><div class="grid-content bg-purple">1.3</div></el-col>
       </el-row>
@@ -16,6 +21,36 @@
 </template>
 
 <script>
+import CarPie from '@/components/charts/CarPie'
+export default {
+  components: {
+    CarPie
+  },
+  data () {
+    return {
+      shopServicesDesc: '门店服务总人数',
+      shopServicesData: []
+    }
+  },
+  mounted: function () {
+    // 获取门店服务总人数
+    this.getShopServiceData()
+  },
+  methods: {
+    // 获取门店服务总人数
+    getShopServiceData: function () {
+      let url = '/shopservices/?city=' + this.common.city.id + '&date=' + this.common.date + '&shopid=' + this.$route.params.shopid
+      this.axios.get(url).then((response) => {
+        this.shopServicesData = {
+          columns: ['name', 'num'],
+          rows: response.data
+        }
+      }).catch((e) => {
+        console.log(e)
+      })
+    }
+  }
+}
 </script>
 
 <style>
